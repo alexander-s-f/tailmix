d96e6359288baea5df811df8abc10cbad723ee3e
Alexander <alexander.s.fokin@gmail.com>
2025-08-23 18:59:33 +0300

    Add `inspect` method to `facade_builder` for improved debugging output of component and elements

--- lib/tailmix/runtime/facade_builder.rb
+++ lib/tailmix/runtime/facade_builder.rb
@@ -10,6 +10,12 @@ module Tailmix
               attributes_for(element_name, runtime_dimensions)
             end
           end
+
+          def inspect
+            component_name = @component_instance.class.name || "AnonymousComponent"
+            elements_list = @definition.elements.keys.join(", ")
+            "#<Tailmix::UI for #{component_name} elements=[#{elements_list}] dimensions=#{@dimensions.inspect}>"
+          end
         end
       end
     end
