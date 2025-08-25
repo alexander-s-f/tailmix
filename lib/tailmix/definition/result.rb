09debe6c36e3d094a6b4cb23edb72fa8b29489f2
Alexander <alexander.s.fokin@gmail.com>
2025-08-25 16:24:54 +0300

    Refactor `result` to ensure `to_h` consistency across attributes, dimensions, and stimulus. Add `to_h` implementations for core structs.

--- lib/tailmix/definition/result.rb
+++ lib/tailmix/definition/result.rb
@@ -17,7 +17,18 @@ module Tailmix
           {
             name: name,
             attributes: attributes.to_h,
-            dimensions: dimensions,
+            dimensions: dimensions.transform_values do |dimension|
+              dimension.transform_values do |value|
+                case value
+                when Variant
+                  value.to_h
+                when Hash
+                  value.transform_values { |v| v.respond_to?(:to_h) ? v.to_h : v }
+                else
+                  value
+                end
+              end
+            end,
             stimulus: stimulus.to_h
           }
         end
@@ -38,9 +49,30 @@ module Tailmix
         end
       end
 
-      Attributes = Struct.new(:classes, keyword_init: true)
-      Stimulus = Struct.new(:definitions, keyword_init: true)
-      Action = Struct.new(:action, :mutations, keyword_init: true)
+      Attributes = Struct.new(:classes, keyword_init: true) do
+        def to_h
+          {
+            classes: classes
+          }
+        end
+      end
+
+      Stimulus = Struct.new(:definitions, keyword_init: true) do
+        def to_h
+          {
+            definitions: definitions
+          }
+        end
+      end
+
+      Action = Struct.new(:action, :mutations, keyword_init: true) do
+        def to_h
+          {
+            action: action,
+            mutations: mutations
+          }
+        end
+      end
     end
   end
 end
