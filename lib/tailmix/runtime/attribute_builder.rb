6d2c1bd9e5a585ecd79ed84393cd33b62e8deaa1
Alexander <alexander.s.fokin@gmail.com>
2025-09-07 22:02:32 +0300

    Refactor `AttributeBuilder` to modularize attribute handling, add model bindings support, and improve code clarity.

--- lib/tailmix/runtime/attribute_builder.rb
+++ lib/tailmix/runtime/attribute_builder.rb
@@ -2,6 +2,7 @@
 
 module Tailmix
   module Runtime
+    # Builder that calculates the complete set of HTML attributes for a single element based on its definition (DSL) and the component's current state.
     class AttributeBuilder
       def initialize(element_def, state, context)
         @element_def = element_def
@@ -10,15 +11,12 @@ module Tailmix
       end
 
       def build
-        attributes = HTML::Attributes.new(
-          { class: @element_def.attributes.classes },
-          element_name: @element_def.name,
-          context: @context
-        )
+        attributes = create_base_attributes
 
         apply_dimensions(attributes)
         apply_compound_variants(attributes)
         apply_attribute_bindings(attributes)
+        apply_model_bindings(attributes)
         apply_event_bindings(attributes)
 
         attributes
@@ -26,6 +24,15 @@ module Tailmix
 
       private
 
+      def create_base_attributes
+        HTML::Attributes.new(
+          { class: @element_def.attributes.classes },
+          element_name: @element_def.name,
+          context: @context
+        )
+      end
+
+      # Applies classes and data/aria attributes from `dimension`.
       def apply_dimensions(attributes)
         @element_def.dimensions.each do |name, dim_def|
           value = @state[name] || dim_def[:default]
@@ -35,50 +42,55 @@ module Tailmix
           next unless variant_def
 
           attributes.classes.add(variant_def.classes)
-          attributes.data.merge!(variant_def.data)
-          attributes.aria.merge!(variant_def.aria)
+          attributes.data.merge!(variant_def.data) if variant_def.data
+          attributes.aria.merge!(variant_def.aria) if variant_def.aria
         end
       end
 
-      def apply_attribute_bindings(attributes)
-        if @element_def.attribute_bindings&.any?
-          @element_def.attribute_bindings.each do |attr_name, state_key|
-            value = @state[state_key] # || dim_def[:default]
-            attributes[attr_name] = value if value
-          end
-        end
-      end
-
-      def apply_event_bindings(attributes)
-        if @element_def.event_bindings&.any?
-          action_definitions = []
-          payload_definitions = []
-
-          @element_def.event_bindings.each do |binding|
-            action_definitions << "#{binding[:event]}->#{binding[:action]}"
-            payload_definitions << binding[:with].to_json if binding[:with]
-          end
-
-          attributes.data.add(tailmix_action: action_definitions.join(" "))
-          attributes.data.add(tailmix_action_payload: payload_definitions.join(" ")) if payload_definitions.any?
-        end
-      end
-
+      # Applies classes and data/aria attributes from `compound_variant`.
       def apply_compound_variants(attributes)
         @element_def.compound_variants.each do |cv|
-          conditions = cv[:on]
+          next unless cv[:on].all? { |key, value| @state[key] == value }
+
           modifications = cv[:modifications]
-
-          match = conditions.all? do |key, value|
-            @state[key] == value
-          end
-
-          if match
-            attributes.classes.add(modifications.classes)
-            attributes.data.merge!(modifications.data)
-            attributes.aria.merge!(modifications.aria)
-          end
+          attributes.classes.add(modifications.classes)
+          attributes.data.merge!(modifications.data) if modifications.data
+          attributes.aria.merge!(modifications.aria) if modifications.aria
         end
+      end
+
+      # Applies one-way attribute bindings (`bind :src, to: :url`).
+      def apply_attribute_bindings(attributes)
+        @element_def.attribute_bindings&.each do |attr_name, state_key|
+          value = @state[state_key]
+          attributes[attr_name] = value if value
+        end
+      end
+
+      # Applies two-way bindings (`model :value, to: :query`).
+      def apply_model_bindings(attributes)
+        @element_def.model_bindings&.each do |attr_name, binding_def|
+          state_key = binding_def[:state]
+          value = @state[state_key]
+          attributes[attr_name] = value if value
+
+          # We are adding data attributes that will "bring to life" the client-side JS.
+          attributes.data.add("tailmix-model-attr": attr_name)
+          attributes.data.add("tailmix-model-state": state_key)
+          attributes.data.add("tailmix-model-event": binding_def[:event])
+          attributes.data.add("tailmix-model-action": binding_def[:action]) if binding_def[:action]
+        end
+      end
+
+      # Applies event handlers (`on :click, :save`).
+      def apply_event_bindings(attributes)
+        return unless @element_def.event_bindings&.any?
+
+        action_string = @element_def.event_bindings.map { |b| "#{b[:event]}->#{b[:action]}" }.join(" ")
+        with_map = @element_def.event_bindings.map { |b| b[:with] }.compact.reduce({}, :merge)
+
+        attributes.data.add(tailmix_action: action_string)
+        attributes.data.add(tailmix_action_with: with_map.to_json) unless with_map.empty?
       end
     end
   end
