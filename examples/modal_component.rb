19a6ebadb4e67364a38d412f5655e7c83b7130f7
Alexander <alexander.s.fokin@gmail.com>
2025-09-13 23:18:53 +0300

    Enhance modal component with user-specific states, actions, and reactions. Add support for dynamic counter, status updates, and user data fetching. Include new interactive elements and macros for streamlined behavior management.

--- examples/modal_component.rb
+++ examples/modal_component.rb
@@ -3,6 +3,10 @@
 require_relative "../lib/tailmix"
 require_relative "helpers"
 
+class UserCart
+  # include Tailmix::Service ?
+end
+
 class ModalComponent
   include Tailmix
   attr_reader :ui
@@ -10,14 +14,64 @@ class ModalComponent
   tailmix do
     plugin :auto_focus, on: :open_button, delay: 100
     state :open, default: false, toggle: true
+    state :user_id, default: nil
+    state :user_name, default: nil
+    state :user_email, default: nil
     state :value, default: ""
     state :text, default: ""
+    state :counter, default: 0
+    state :status_text, default: "Ready"
 
-    react on: :value do |r|
-      # TODO: new dsl & interpreter
+    # --- Macros ---
+    macro :update_status, :text do |text|
+      set(:status_text, text)
+      log("Status updated to:", text)
+    end
+
+    # --- Actions ---
+
+    action :report do
+      if?(state(:counter).gt(5)) do
+        expand_macro(:update_status, concat("Counter is high: ", state(:counter)))
+      end
+    end
+
+    action :increment_and_report do
+      increment(:counter)
+      expand_macro(:update_status, concat("Counter is high: ", state(:counter)))
+      call(:report)
+    end
+
+    action :reset do
+      set(:counter, 0)
+      expand_macro(:update_status, "Counter has been reset.")
+    end
+
+    # --- Reactions ---
+    reaction on: :open do
+      if?(state(:open)) do
+        expand_macro(:update_status, "Modal opened.")
+      end
+      log("user_id:", state(:user_id))
+      fetch("/user.json", params: { user_id: state(:user_id) }, service: UserCart) do |response|
+        if?(response.success?) do
+          log("result:", response.result)
+          log("result(:name):", response.result(:name))
+          set(:user_name, response.result(:name))
+          set(:user_email, response.result(:email))
+        end
+      end
     end
 
     element :container do
+    end
+
+    element :reset_button do
+      on :click, :reset
+    end
+
+    element :increment_button do
+      on :click, :increment_and_report
     end
 
     element :open_button do
@@ -57,8 +111,20 @@ class ModalComponent
       bind :text, to: :value
     end
 
+    element :counter, "text-xl p-4 min-h-[2em]" do
+      bind :text, to: :counter
+    end
+
     element :text, "text-xl p-4 min-h-[2em]" do
       bind :text, to: :text
+    end
+
+    element :user_name, "text-xl p-4 min-h-[2em]" do
+      bind :text, to: :user_name
+    end
+
+    element :user_email, "text-xl p-4 min-h-[2em]" do
+      bind :text, to: :user_email
     end
 
     element :close_button, "absolute top-2 right-2 p-1 text-gray-500 rounded-full cursor-pointer" do
@@ -69,10 +135,11 @@ class ModalComponent
     element :body, "p-4"
   end
 
-  def initialize(open: false, id: nil)
-    @ui = tailmix(open: open, id: id)
+  def initialize(open: false, user_id: nil, id: nil)
+    @ui = tailmix(open: open, user_id: user_id, id: id)
   end
 end
+
 
 
 modal = ModalComponent.new(open: false, id: :user_profile_modal)
